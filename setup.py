3bc1fe8cc4ae1cc16af2e2565534f780a0e18bb0
vinal-2 <97253630+vinal-2@users.noreply.github.com>
2024-01-12 00:07:22 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gtfobins-scan',
-    version='1.0.0',
-    description='Vulnerability Scanner for GTFOBins',
+    name="gtfobins-scan",
+    version="2.0",
+    description="Vulnerability Scanner for GTFOBins",
     packages=find_packages(),
-    author='Vinal',
-    url='https://github.com/vinal-2/GTFObins',
-    license='GPL-3.0',
-    scripts=['gtfobins_scan.sh'],
-    
+    install_requires=[
+        "requests",
+    ],
+    author="Vinal",
+    url="https://github.com/vinal-2/GTFObins",
+    license="GPL-3.0",
     entry_points={
-        'console_scripts': [
-            'gtfobins-scan=gtfobins_scan:main',
+        "console_scripts": [
+            "gtfobins-scan=gtfobins_scan:main",
         ],
+    },
+    package_data={
+        "": ["gtfobins_scan.sh"],
     },
 )
